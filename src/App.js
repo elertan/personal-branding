9541b2d578494fcf84d52db80c3ea6b772614df2
elertan <denkievits@gmail.com>
2018-05-09 01:06:36 +0200

    Styling changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './components/Header';
 import Landing from './components/Landing';
+import About from './components/About';
 import Footer from './components/Footer';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
                 <Landing>
                     <Header />
                 </Landing>
+                <About />
                 <Footer />
             </div>
         );
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class About extends React.Component {
     render() {
         return (
-
+            <div style={{height: 700}}></div>
         );
     }
 }
